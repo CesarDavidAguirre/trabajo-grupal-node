3e51614a927a0ac4d9f88966efac66493f5eec42
kaiser <cesar.aguirre@naranja.com>
2020-11-19 18:13:48 -0300

    fix post api

--- src/requestDNI/handler/createDNI.js
+++ src/requestDNI/handler/createDNI.js
@@ -2,8 +2,8 @@ const { commandMapper } = require('ebased/handler');
 const inputMode = require('ebased/handler/input/commandApi');
 const outputMode = require('ebased/handler/output/commandApi');
 
-const { domain } = require('../domain/XXX');
+const { domain } = require('../domain/createDNI');
 
-module.exports.handler = async (events, context) => {
+module.exports.post = async (events, context) => {
   return commandMapper({ events, context }, inputMode, domain, outputMode);
 };
